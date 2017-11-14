d2308244981a4a2c9279f112e2d6131de901614a
Evan Candler <emcandler21@gmail.com>
2017-11-14 03:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,7 @@ function happyHolidaysTo(name) {
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
-function holidayCountdown(days, holiday)
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
+
